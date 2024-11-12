cc191dce95f5a4f5e1cc2ee9cc7378ff227ff26b
YanisDiss <lefortyanis27@gmail.com>
2024-11-12 10:56:22 +0100

    ajout des image + systeme de colonne + easing

--- jeu.py
+++ jeu.py
@@ -15,45 +15,119 @@
 """
 
 import pygame
+import random
+import math
 
-GRISCLAIR  = (  214, 214, 214)
-BLEU       = (153, 164,   242)
+BG_COLOR  = (  21, 21, 21)
+MAIN_COLOR       = (153, 164,   242)
 
 X = 0
 Y = 1
 
-FENETRE_LARGEUR = 800
-FENETRE_HAUTEUR = 600
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
 
-BORD_LARGEUR = 100
+PLAYER_SIZE = 50
+PLAYER_SIZE_2 = (PLAYER_SIZE, PLAYER_SIZE)
 
-PLAYER_TAILLE = 50
-PLAYER_TAILLE_2 = (PLAYER_TAILLE, PLAYER_TAILLE)
+COL_NUMBERS = 7 # Nombre de colonnes
+COL_SIZE = WINDOW_WIDTH/COL_NUMBERS # taille d'une colonne
 
-NOMBRE_COLONNES = 5
-TOUCHE_DROITE = pygame.K_RIGHT
-TOUCHE_GAUCHE = pygame.K_LEFT
+KEY_RIGHT = pygame.K_RIGHT
+KEY_LEFT = pygame.K_LEFT
 
-AGAUCHE = -1
-ADROITE = 1
+TO_THE_LEFT = -1
+TO_THE_RIGHT = 1
 
-def move(sens):
-    player_position[X] += FENETRE_LARGEUR / NOMBRE_COLONNES * sens
-    if (player_position[X] < 0): player_position[X]
-    elif (player_position[X] + PLAYER_TAILLE >= FENETRE_LARGEUR):
-        player_position[X] = FENETRE_LARGEUR - PLAYER_TAILLE
+SANIC_IMAGE = pygame.image.load("assets/images/sanic.png")
+SANIC_IMAGE = pygame.transform.scale(SANIC_IMAGE, (50, 50))
+
+TERMINATOR_IMAGE = pygame.image.load("assets/images/terminator.png")
+TERMINATOR_IMAGE = pygame.transform.scale(TERMINATOR_IMAGE, (80, 80))
+TERMINATOR_IMAGE = pygame.transform.rotate(TERMINATOR_IMAGE, 180)
+
 
 pygame.init()
 
-fenetre_taille = (FENETRE_LARGEUR, FENETRE_HAUTEUR)
-fenetre = pygame.display.set_mode(fenetre_taille)
+windowDimensions = (WINDOW_WIDTH, WINDOW_HEIGHT)
+window = pygame.display.set_mode(windowDimensions)
 
-fenetre.fill(GRISCLAIR)
-
-player_position = [400, 500]
+window.fill(BG_COLOR)
 
 fini = False
 temps = pygame.time.Clock()
+
+def col_to_pos(col):
+    return col * COL_SIZE + COL_SIZE / 2
+
+def create_entity(col: int, y: int, velocity: float, acceleration: float, skin, damage: float):
+    entity = {
+        "col": col,
+        "x": col_to_pos(col),
+        "y": y,
+        "velocity": velocity,
+        "acceleration": acceleration,
+        "skin": skin,
+        "damage": damage
+    }
+
+    if(skin):
+        entity["x"] += skin.get_size()[0]
+
+    return entity;
+
+
+
+def create_player():
+    return create_entity(COL_NUMBERS // 2, 500, 0.1, 0, None, 0.0)
+
+player = create_player()
+
+entities = []
+
+def move_player_animation(delta_t, entity):
+    goal = col_to_pos(entity["col"])
+    
+    if(player["x"] == goal):
+        return
+
+    delta_x = goal - entity["x"]
+
+    if(delta_x > 0):
+        entity["x"] += int(entity["velocity"] * delta_t)
+    elif(delta_x < 0):
+        entity["x"] -= int(entity["velocity"] * delta_t)
+        
+    if(player["x"] > goal - COL_SIZE / 2 and player["x"] < goal + COL_SIZE / 2):
+        player["x"] = goal
+
+
+def move(sens):
+    global col_x
+    
+    if((player["col"] <= 1 and sens == TO_THE_LEFT) or (player["col"] >= COL_NUMBERS - 2 and sens == TO_THE_RIGHT)):
+        return
+
+
+    player["col"] += sens
+
+def draw_entities():
+    for entity in entities:
+        window.blit(entity["skin"], (entity["x"], entity["y"]))
+
+def move_entities(delta):
+    for entity in entities:
+        entity["velocity"] += entity["acceleration"] * delta
+        entity["y"] += entity["velocity"] * delta
+
+        if(entity["y"] > WINDOW_HEIGHT):
+            entities.remove(entity)
+
+def spawn_entities():
+    if random.random() > 0.99:
+        entities.append(
+            create_entity(random.randint(0, COL_NUMBERS - 3), -100, 0, 0.0004, TERMINATOR_IMAGE, 0.0)
+        )
 
 while not fini:
     #--- Traiter entrées joueur
@@ -61,31 +135,35 @@ while not fini:
         if evenement.type == pygame.QUIT:
             fini = True
         elif evenement.type == pygame.KEYDOWN:
-            if evenement.key == TOUCHE_DROITE:
-                move(ADROITE)
-            elif evenement.key == TOUCHE_GAUCHE:
-                move(AGAUCHE)
+            if evenement.key == KEY_RIGHT:
+                move(TO_THE_RIGHT)
+            elif evenement.key == KEY_LEFT:
+                move(TO_THE_LEFT)
 
 
-    fenetre.fill(GRISCLAIR)
+    window.fill(BG_COLOR)
 
     # affichage du joueur
-    pygame.draw.rect(fenetre, BLEU, (
-        (player_position[0] - PLAYER_TAILLE/2, player_position[1]), # pour qu'il soit a l'exact milieu de l'ecran
-        PLAYER_TAILLE_2), 
+    pygame.draw.rect(window, MAIN_COLOR, (
+        (player["x"] - PLAYER_SIZE/2, player["y"]), # pour qu'il soit a l'exact milieu de l'ecran
+        PLAYER_SIZE_2), 
         16, 3) #pour les bord arrondis et l'outline
 
 
     # pour les bords
-    pygame.draw.rect(fenetre, BLEU, ((0, 0), (BORD_LARGEUR, FENETRE_HAUTEUR)))
-    pygame.draw.rect(fenetre, BLEU, ((FENETRE_LARGEUR - BORD_LARGEUR, 0), (BORD_LARGEUR, FENETRE_HAUTEUR)))
+    pygame.draw.rect(window, MAIN_COLOR, ((0, 0), (COL_SIZE, WINDOW_HEIGHT)))
+    pygame.draw.rect(window, MAIN_COLOR, ((WINDOW_WIDTH - COL_SIZE, 0), (COL_SIZE, WINDOW_HEIGHT)))
 
+    #--- 60 images par seconde
+    delta = temps.tick(60)
+
+    move_entities(delta)
+    draw_entities()
+    spawn_entities()
+    move_player_animation(delta, player)
 
     #--- Afficher (rafraîchir) l'écran
     pygame.display.flip()
-
-    #--- 60 images par seconde
-    temps.tick(60)
 
 
 pygame.display.quit()
